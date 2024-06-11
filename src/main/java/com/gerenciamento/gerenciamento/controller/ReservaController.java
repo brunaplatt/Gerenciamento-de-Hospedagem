4304b7ad56229dd83707eb6eaf8519f6c672b3cb
brunaplatt <111389173+brunaplatt@users.noreply.github.com>
2024-06-11 20:20:10 -0300

    ajustes html

--- src/main/java/com/gerenciamento/gerenciamento/controller/ReservaController.java
+++ src/main/java/com/gerenciamento/gerenciamento/controller/ReservaController.java
@@ -5,6 +5,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.gerenciamento.gerenciamento.entity.Reserva;
 import com.gerenciamento.gerenciamento.service.ReservaService;
